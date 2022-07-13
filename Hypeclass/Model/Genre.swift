19558f328e78e7104c1dde9b228470a2e69a4c19
Luke_Kong <kongthn@gmail.com>
2022-07-13 12:19:03 +0900

    [ADD] Genre enum

---
+++ Hypeclass/Model/Genre.swift
@@ -0,0 +1,18 @@
+//
+//  Genre.swift
+//  Hypeclass
+//
+//  Created by GOngTAE on 2022/07/13.
+//
+
+import Foundation
+
+enum Genre {
+    case choreography
+    case hiphop
+    case waacking
+    case locking
+    case house
+    case popping
+    case vouging
+}
