1d4d792760b3c5e50c9c80ea411f1fe73f794211
jeohong <jeohong59@gmail.com>
2022-07-14 17:57:33 +0900

    [FEAT] SearchButton 클릭시 화면 전환 구현

--- Hypeclass/Source/Main/SearchViewController.swift
+++ Hypeclass/Source/Main/SearchViewController.swift
@@ -15,20 +15,38 @@ class SearchViewController: BaseViewController {
         searchBar.becomeFirstResponder()
         searchBar.tintColor = .white
         searchBar.searchTextField.textColor = .white
+        
         return searchBar
     }()
     
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationBarUI()
         configureUI()
+        
+        searchBar.delegate = self
     }
     
     //MARK: - Selectors
     //MARK: - Helpers
     func configureUI() {
         //레이아웃 구성
+    }
+    
+    func setNavigationBarUI() {
         self.navigationItem.titleView = searchBar
+    }
+}
+
+// SearchBar Delegate
+extension SearchViewController: UISearchBarDelegate {
+    // SearchButton Clicked
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        guard let searchText = searchBar.text else { return }
+        let searchDetailVC = SearchDetailViewController()
+        self.navigationController?.pushViewController(searchDetailVC, animated: true)
+        
     }
 }
 
