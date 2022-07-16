44211186d9c1fb5e007875237b6f1a8974fd6037
jeohong <jeohong59@gmail.com>
2022-07-16 15:45:23 +0900

    [Fix] 코드의 오타를 수정하였습니다.

--- Hypeclass/Util/Extension/UIColor.swift
+++ Hypeclass/Util/Extension/UIColor.swift
@@ -21,5 +21,5 @@ extension UIColor {
     
     class var background: UIColor { UIColor(hex: 0x181818) }
     class var accent: UIColor { UIColor(hex: 0xC7FD51) }
-    class var searchBarBackgroud: UIColor{ UIColor(hex: 0x2D2C38) }
+    class var searchBarBackground: UIColor{ UIColor(hex: 0x2D2C38) }
 }
