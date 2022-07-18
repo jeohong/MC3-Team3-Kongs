55720583cde0a0fb4379a60a260ed4b90a0945ec
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-07-18 14:52:47 +0900

    [Refactor] Constraint Helper 함수로 분리 및 행간 분리 수정

--- Hypeclass/Source/Main/MainViewController.swift
+++ Hypeclass/Source/Main/MainViewController.swift
@@ -9,48 +9,42 @@ import UIKit
 
 class MainViewController: BaseViewController {
     //MARK: - Properties
+    
     let titleLabel: UILabel = {
         let label = UILabel()
-        
         label.text = "댄서들의 클래스를 확인해보세요"
         label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
         label.adjustsFontSizeToFitWidth = true
         
         //MARK: - TODO: func textRect(라벨 박스 크기 확인)
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     let searchButton: UIButton = {
         var config = UIButton.Configuration.gray()
-        
         config.buttonSize = .large
         config.titleAlignment = .leading
         config.title = "찾고자 하는 댄서나 장르를 검색해보세요"
         config.background = .listSidebarCell()
-        
-        
         //MARK: - 돋보기 이미지 구성
+        
         config.image = UIImage(systemName: "magnifyingglass")
         config.imagePadding = 4
         config.imagePlacement = .leading
-        
         config.baseForegroundColor = UIColor(hex: 0x7A7A7A)
         config.baseBackgroundColor = UIColor(hex: 0x2D2C38)
-        
         let button = UIButton (
             configuration: config, primaryAction: UIAction(handler: { _ in
             print("MainViewController -> SearchViewController")
             //MARK: TO_DO: searchView 연결
 //            let searchViewController = SearchViewController()
 //            UINavigationController?.pushViewController(searchViewController, animated: true)
-        })
+            })
         )
-        
         button.contentHorizontalAlignment = .leading
         button.translatesAutoresizingMaskIntoConstraints = false
-        
-        
         return button
     }()
 
@@ -61,19 +55,25 @@ class MainViewController: BaseViewController {
         
         view.addSubview(titleLabel)
         view.addSubview(searchButton)
+        // MARK: - AutoLayout
         
-        let safeArea = self.view.safeAreaLayoutGuide
-
-        self.titleLabel.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 60).isActive = true
-        self.titleLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 25).isActive = true
-        
-        self.searchButton.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 120).isActive = true
-        self.searchButton.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 25).isActive = true
-        self.searchButton.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -25).isActive = true
-        
+        titleConstraint()
+        searchButtonConstraint()
     }
     //MARK: - Selectors
     //MARK: - Helpers
+    private func titleConstraint() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        self.titleLabel.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 60).isActive = true
+        self.titleLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 25).isActive = true
+    }
+    
+    private func searchButtonConstraint() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        self.searchButton.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 120).isActive = true
+        self.searchButton.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 25).isActive = true
+        self.searchButton.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -25).isActive = true
+    }
 }
 
 
