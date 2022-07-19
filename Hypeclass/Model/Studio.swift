94afab4bd015a6049493de486348ef3107faa06e
Luke_Kong <kongthn@gmail.com>
2022-07-19 21:32:56 +0900

    [RENAME] Url -> URL

--- Hypeclass/Model/Studio.swift
+++ Hypeclass/Model/Studio.swift
@@ -11,7 +11,8 @@ struct Studio: Codable {
     let id: String
     let name: String
     let description: String?
+    let instagramURL: String?
+    let youtubeURL: String?
 //    let tags: [String]
     let classes: [DanceClass]
-    let platformURLs: [String] //URL 내에 플랫폼 정보가 담겨져있어 URL 자체만 담도록 변경
 }
