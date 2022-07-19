4cb6e6dde6c4c0c42fde54fed9612df3a6d90ed7
jeohong <jeohong59@gmail.com>
2022-07-19 14:53:30 +0900

    [FEAT] SearchDetailView NavigationBar 수정

--- Hypeclass/Source/Main/SearchDetailViewController.swift
+++ Hypeclass/Source/Main/SearchDetailViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 class SearchDetailViewController: BaseViewController {
     
     //MARK: - Properties
+    let searchLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        
+        return label
+    }()
     
-    let searchBar = SearchBar()
     let separator = Separator()
     
     //MARK: - LifeCycle
@@ -30,14 +36,15 @@ class SearchDetailViewController: BaseViewController {
         // Separator Layout
         self.view.addSubview(separator)
         separator.translatesAutoresizingMaskIntoConstraints = false
-        separator.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 12).isActive = true
+        separator.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
         separator.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
         separator.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -18).isActive = true
         separator.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 18).isActive = true
     }
     
     func setNavigationBarUI() {
-        self.navigationItem.titleView = searchBar
+        searchLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.navigationController?.navigationBar.topItem?.title = searchLabel.text
     }
 }
 
@@ -47,11 +54,11 @@ import SwiftUI
 
 struct SearchDetailViewControllerRepresentable: UIViewControllerRepresentable {
     typealias UIViewControllerType = SearchDetailViewController
-    
+
     func makeUIViewController(context: Context) -> SearchDetailViewController {
         return SearchDetailViewController()
     }
-    
+
     func updateUIViewController(_ uiViewController: SearchDetailViewController, context: Context) {}
 }
 
