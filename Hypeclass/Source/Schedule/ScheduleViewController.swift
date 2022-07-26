03cda966d1fd90c095cf3dffdd4a2ebee1cf8202
Luke_Kong <kongthn@gmail.com>
2022-07-26 16:15:59 +0900

    [FEAT] 모델 수정

--- Hypeclass/Source/Schedule/ScheduleViewController.swift
+++ Hypeclass/Source/Schedule/ScheduleViewController.swift
@@ -227,11 +227,11 @@ class ScheduleViewController: BaseViewController {
             weekSchedules[$0.0].forEach { schedule in
                 let dancerName = schedule.dancerName
                 let studioName = schedule.studioName
-                let startTime = "\(schedule.startTime.get(.hour)):\(schedule.startTime.get(.minute))"
-                let endTime = "\(schedule.endTime.get(.hour)):\(schedule.endTime.get(.minute))"
+                let startTime = "\(schedule.startTime?.get(.hour)):\(schedule.startTime.get(.minute))"
+                let endTime = "\(schedule.endTime?.get(.hour)):\(schedule.endTime.get(.minute))"
 
                 scheduleViewWidth = (scheduleCollectionView.frame.width - 15) / 2
-                let scheduleView = ScheduleView(frame: .zero, dancerName: dancerName, studioName: studioName, startTime: startTime, endTime: endTime, viewWidth: scheduleViewWidth)
+                let scheduleView = ScheduleView(frame: .zero, dancerName: dancerName ?? <#default value#>, studioName: studioName, startTime: startTime, endTime: endTime, viewWidth: scheduleViewWidth)
                 scheduleView.translatesAutoresizingMaskIntoConstraints = false
                 scheduleView.widthAnchor.constraint(equalToConstant: scheduleViewWidth).isActive = true
                 scheduleView.heightAnchor.constraint(equalToConstant: scheduleCollectionView.frame.height / 8).isActive = true
