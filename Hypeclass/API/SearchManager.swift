3c57beeb4feb2281e05bb68145906ea56080ae2e
jeohong <jeohong59@gmail.com>
2022-07-26 22:16:08 +0900

    [FEAT] SearchManager 구현

---
+++ Hypeclass/API/SearchManager.swift
@@ -0,0 +1,45 @@
+//
+//  SearchManager.swift
+//  Hypeclass
+//
+//  Created by Hong jeongmin on 2022/07/26.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class SearchManager {
+    static let shared = SearchManager()
+    
+    func requestSearchDancer(dancerSearch: String) async throws -> [Dancer]? {
+        let snapshot = try await Constant.dancerRef.whereField("name", isGreaterThanOrEqualTo: dancerSearch).getDocuments()
+        
+        let searchDancer = snapshot.documents.compactMap { document in
+            try? document.data(as: Dancer.self)
+        }
+        
+        return searchDancer
+    }
+    
+    func requestSearchStudio(studioSearch: String) async throws -> [Studio]? {
+        let snapshot = try await Constant.studioRef.whereField("name", isGreaterThan: studioSearch).getDocuments()
+        
+        let searchStudio =  snapshot.documents.compactMap { document in
+            try? document.data(as: Studio.self)
+        }
+        
+        return searchStudio
+    }
+    
+    func requestSearchGenre(genreSearch: String) async throws -> [Dancer]? {
+        
+        let snapshot = try await Constant.dancerRef.whereField("genres", arrayContains: genreSearch).getDocuments()
+        
+        let searchGenre = snapshot.documents.compactMap { document in
+            try? document.data(as: Dancer.self)
+        }
+        
+        return searchGenre
+    }
+}
