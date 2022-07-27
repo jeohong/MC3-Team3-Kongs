dc3600a0b569dbb8bae7fe75a2cc72989442ea17
Cap0011 <59128435+Cap0011@users.noreply.github.com>
2022-07-27 22:00:05 +0900

    [FEAT] 구독 커스텀 탭바 구현, Firebase 연결

--- Hypeclass/API/StudioManager.swift
+++ Hypeclass/API/StudioManager.swift
@@ -10,6 +10,8 @@ import Foundation
 class StudioManager {
     static let shared = StudioManager()
     
+    static var myStudios: [Studio]?
+    
     func createStudio(id: String, name: String, dancers: [String]) {
         let studio = Studio(id: id, name: name, description: "\(name) description", instagramURL: nil, youtubeURL: nil, dancers: dancers)
         do {
@@ -22,6 +24,14 @@ class StudioManager {
     func requestStudiosBy(dancerName name: String) async throws -> [Studio]? {
         let snapshot = try await Constant.studioRef.whereField("dancers", arrayContains: name).getDocuments()
         
+        return snapshot.documents.compactMap { document in
+            try? document.data(as: Studio.self)
+        }
+    }
+    
+    func requestStudiosBy(studioIDs ids: [String]) async throws -> [Studio]? {
+        let snapshot = try await Constant.studioRef.whereField("id", in: ids).getDocuments()
+
         return snapshot.documents.compactMap { document in
             try? document.data(as: Studio.self)
         }
