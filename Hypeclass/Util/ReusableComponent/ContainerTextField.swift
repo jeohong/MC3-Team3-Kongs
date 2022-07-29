a589fd14251e0b881318f804db19f3fc070e4a96
Luke_Kong <kongthn@gmail.com>
2022-07-30 02:23:36 +0900

    [RENAME] UIColor.serchBarBackground -> container

---
+++ Hypeclass/Util/ReusableComponent/ContainerTextField.swift
@@ -0,0 +1,20 @@
+//
+//  ContainerTextField.swift
+//  Hypeclass
+//
+//  Created by GOngTAE on 2022/07/30.
+//
+
+import UIKit
+
+class ContainerTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
