b01ed4abd3d7750f99f94bffbe8a6a11d788c60e
Luke_Kong <kongthn@gmail.com>
2022-07-30 01:45:36 +0900

    [ADD] Reusable CallToAction Button

---
+++ Hypeclass/Util/ReusableComponent/CTAButton.swift
@@ -0,0 +1,27 @@
+//
+//  CTAButton.swift
+//  Hypeclass
+//
+//  Created by GOngTAE on 2022/07/30.
+//
+
+import UIKit
+
+class CTAButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(title: String) {
+        super.init(frame: CGRect.zero)
+        layer.cornerRadius = 8
+        backgroundColor = .accent
+        setTitleColor(.black, for: .normal)
+        setTitle(title, for: .normal)
+    }
+}
